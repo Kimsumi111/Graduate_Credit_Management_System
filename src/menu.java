47fd74630d7ee2b329739dd6ed30b8304806df1d
rlatn <rlatn@kimsumi>
2023-03-19 23:04:08 +0900

    removed hello.java and management.java

---
+++ src/menu.java
@@ -0,0 +1,13 @@
+
+public class menu {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Credit" );
+		System.out.println("2. Delete Credit");
+		System.out.println("3. Edit Credit" );
+		System.out.println("4. View Credit" );
+		System.out.println("5. Exit" );
+
+	}
+
+}
